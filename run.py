18152a636473bffc7776568e3a3bc9ff09789599
r3b3l-dev <wayne.manning@tuta.io>
2021-11-27 22:11:48 +0000

    Add start game function that initiates new game

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+
+def start_game():
+    """
+    Initiates a new game. Allows the player to set the board size and the
+    number of ships in the game. Initialises the boards and resets the scores.
+    """
+
+    board_size = 5
+    amount_ships = 4
+    print("-----------------------------------")
+    print("      Welcome to BATTLESHIPS!!     ")
+    print(f" Board Size: {board_size}. Number of Ships: {amount_ships}")
+    print(" Top left corner is row: 0, col: 0")
+    print("-----------------------------------")
+    player_name = input("What is your name? \n")
+    print("-----------------------------------")
+
+start_game()
+
